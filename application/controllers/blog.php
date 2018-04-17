67eb64a8ece22ac3480dde0aefc7e343195ac006
DESKTOP-3UB7BKR\ASUS <claranadya123@gmail.com>
2018-04-17 22:34:38 +0700

    tugas 06

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -1,21 +1,61 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class blog extends CI_Controller {
+class Blog extends CI_Controller {
 
- public function index() { 
+   public function index() { 
          
          $data['records'] = $this->blog_model->getAll(); 
          $this->load->view('blog_list',$data); 
       } 
   
-          public function add_view() {
-            $data['error'] = "";
+      public function add_view() {
+        $data['error'] = ""; 
+      $this->load->helper(array('form', 'url'));
+      $this->load->library('form_validation');
+      $this->form_validation->set_rules('id', 'Id', 'required');
+      $this->form_validation->set_rules('author', 'Author', 'required');
+      $this->form_validation->set_rules('title', 'Title', 'required');
+      if ($this->form_validation->run() == FALSE)
+      {
          $this->load->view('blog_add_view',$data); 
-      }  
+      }
+      else
+      {
+         $config['upload_path']   = './uploads/';  
+         $config['allowed_types'] = 'gif|jpg|png';  
+         $config['max_size']      = 80000;  
+         $config['max_width']     = 1024;  
+         $config['max_height']    = 768;   
+         $this->load->library('upload', $config); 
+         $this->upload->initialize($config); 
+
+         if ( ! $this->upload->do_upload('image_file')) { 
+            $error = array('error' => $this->upload->display_errors());  
+            $this->load->view('blog_add_view', $error);  
+         } 
+
+         else {  
+            $dataUpload = $this->upload->data();  
+            $data = array(  
+               'id' => $this->input->post('id'), 
+               'author' => $this->input->post('author'), 
+               'date' => $this->input->post('date'), 
+               'title' => $this->input->post('title'), 
+               'content' => $this->input->post('content'), 
+               'image_file' => $dataUpload['file_name']  
+               );  
+            $this->blog_model->insert($data); 
+            redirect('Blog');  
+         } 
+      }
+     }
+       public function byId($id){
+       $data['records'] = $this->blog_model->getOne($id); 
+         $this->load->view('blog_view',$data); 
+   }
   
-      public function add_action() { 
-         
+    public function add_action() { 
          $config['upload_path']   = './uploads/'; 
          $config['allowed_types'] = 'gif|jpg|png'; 
          $config['max_size']      = 80000; 
@@ -41,11 +81,57 @@ class blog extends CI_Controller {
                ); 
                $this->blog_model->insert($data);
             redirect('Blog'); 
-         } 
-      }
+         
+         }
 
-      public function byId($id){
-     $data['records'] = $this->blog_model->getOne($id); 
-         $this->load->view('blog_view',$data); 
+      }
+      public function update_view($id) {
+      $data['error'] = ""; 
+      $this->load->helper(array('form', 'url'));
+      $this->load->library('form_validation');
+      $this->form_validation->set_rules('id', 'Id', 'required');
+      $this->form_validation->set_rules('author', 'Author', 'required');
+      $this->form_validation->set_rules('title', 'Title', 'required');
+      $data['records'] = $this->blog_model->getOne($id);
+      if ($this->form_validation->run() == FALSE)
+      {
+         $this->load->view('blog_update_view',$data); 
+      }
+      else
+      {
+         $config['upload_path']   = './uploads/';  
+         $config['allowed_types'] = 'gif|jpg|png';  
+         $config['max_size']      = 80000;  
+         $config['max_width']     = 1024;  
+         $config['max_height']    = 768;   
+         $this->load->library('upload', $config); 
+         $this->upload->initialize($config); 
+         
+         if ( ! $this->upload->do_upload('image_file')) { 
+            $data['error'] = $this->upload->display_errors();  
+            $this->load->view('blog_update_view', $data);  
+           
+         } 
+
+         else {  
+            $dataUpload = $this->upload->data();  
+            $data = array(  
+               'id' => $this->input->post('id'), 
+               'author' => $this->input->post('author'), 
+               'date' => $this->input->post('date'), 
+               'title' => $this->input->post('title'), 
+               'content' => $this->input->post('content'), 
+               'image_file' => $dataUpload['file_name']  
+               );  
+            $old_id = $this->input->post('old_id');
+            $this->blog_model->update($data,$old_id); 
+            redirect('Blog');  
+         } 
+      }  
    }
+   public function delete_action($id){
+      $this->blog_model->delete($id);
+      redirect('Blog');
    }
+
+}
--- application/views/blog_list.php
+++ application/views/blog_list.php
@@ -27,21 +27,25 @@
       </div>
     </nav>
     <main role="main" class="container">
-    <a href="<?php echo base_url("index.php/Blog/add_view") ?>" class="btn btn-primary">Tambah Blog</a>
- <ul class="list-unstyled">
+      <a href="<?php echo base_url("index.php/Blog/add_view") ?>" class="btn btn-primary">Tambah Blog</a>
+   <ul class="list-unstyled">
+<ul class="list-unstyled">
   <?php foreach ($records as $key => $value): ?>
-   <li class="media">
-    <img class="mr-3" src="<?php echo base_url() ?>uploads/<?php echo $value['image_file'] ?>" alt="Generic placeholder image" width="100px" height="150px">
+    <li class="media">
+   <img class="mr-3" src="<?php echo base_url() ?>uploads/<?php echo $value['image_file'] ?>" alt="Generic placeholder image" width="100px" height="150px">
     <div class="media-body">
     <h6 class="text-muted"><?php echo $value['date'] ?></h6>
       <h5 class="mt-0 mb-1"><?php echo $value['title'] ?></h5>
       <?php echo $value['content'] ?>
       <br>
       <a href="<?php echo base_url('index.php/Blog/byId/'.$value['id']) ?>">View Details</a>
+      <a class="btn btn-sm btn-success" href="<?php echo base_url('index.php/Blog/update_view/'.$value['id']) ?>">Update  </a>
+            <a class="btn btn-sm btn-danger" href="<?php echo base_url('index.php/Blog/delete_action/'.$value['id']) ?>">Delete </a>
     </div>
   </li>
   <?php endforeach ?>
-</ul>  </main>
+</ul>
+    </main>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
